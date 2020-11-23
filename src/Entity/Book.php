decc95c7755c96bad8aa4d4b3ae2f7388ee91db0
benoit17630 <benoit.menier@orange.fr>
2020-11-23 14:24:30 +0100

    creation BDD creation table Product (make:entity) creation table book (a la main)

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -6,43 +6,62 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+// J'ai créé une base de données en modifiant la variable DATABASE_URL
+// du fichier .env.local (que j'ai créé en copiant le fichier .env)
+// puis en utilisant la ligne de commande doctrine:database:create
+
+// J'ai créé une classe Product qui est une entité car elle possède l'annotation
+// @ORM\Entity
+
 /**
  * @ORM\Entity()
  */
 class Book
 {
+    // J'ai créé autant de propriétés que de colonnes voulues dans la table
+    // et j'ai mappé les propriétés avec des annotations et la classe ORM (attention
+    // de ne pas oublier le use correspondant)
 
+    //creation du champ id en disant qu il genere un integer automatiquement
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    //creation du champ titre de type varchar avec 255 caratere de base
     /**
      * @ORM\Column (type="string")
      */
     private $titre;
 
+    //creation du champ auteur
     /**
      * @ORM\Column(type="string")
      */
     private $auteur;
 
+    //creation du champ nombre de page en chiffre entier
     /**
      * @ORM\Column(type="integer")
      */
     private $nombre_de_page;
 
+    //creation du champ date de publication du type date
     /**
      * @ORM\Column(type="date")
      */
     private $date_publication;
 
+    //creation du champ disponible en boolean en lui disant qu il est vrai au depart
     /**
-     * @ORM\Column(type="boolean", options={"default": false})
+     * @ORM\Column(type="boolean", options={"default": true})
      */
-    private $disponible =false ;
+    private $disponible =true ;
 
+
+    //creation du champ resume de type text et qui n est pas obligatoire
     /**
      * @ORM\Column(type="text" , nullable=true)
      */
